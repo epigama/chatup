76b65d64a4a946cc6ea5c9de56b8a3955146a515
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-07-05 21:49:27 +0530

    java file of intro page

---
+++ app/src/main/java/com/example/chatup/intro_page.java
@@ -0,0 +1,14 @@
+package com.example.chatup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class intro_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_page);
+    }
+}
