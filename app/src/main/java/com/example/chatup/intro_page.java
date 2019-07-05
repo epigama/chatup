4aed567d44d81b5edff341b2f449518f28bbf8e5
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-07-05 22:56:19 +0530

    called the button to next activity

--- app/src/main/java/com/example/chatup/intro_page.java
+++ app/src/main/java/com/example/chatup/intro_page.java
@@ -2,13 +2,24 @@ package com.example.chatup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class intro_page extends AppCompatActivity {
+    Button intro_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_page);
+        intro_button=(Button)findViewById(R.id.IntroButton);
+        intro_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
     }
 }
