cedb36945c945e43bf8c4aa334bbf262684a9ad5
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-07-07 08:01:31 +0530

    updated with permissions like media,storage

--- app/src/main/java/com/example/chatup/profile.java
+++ app/src/main/java/com/example/chatup/profile.java
@@ -1,13 +1,92 @@
 package com.example.chatup;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class profile extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    private AlertDialog PermissionAccessDialog;
+    String TAG="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        if (!checkPermission())
+            requestPermission();
+
     }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    //request for permission
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+
+    }
+
+    //request for permissions
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0) {
+
+                    boolean read_storage_Accepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean write_storage_Accepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if (read_storage_Accepted && write_storage_Accepted)
+                        Log.i(TAG, "onRequestPermissionsResult: Permission granted");
+                    else {
+                        Log.i(TAG, "onRequestPermissionsResult: Permsission not granted");
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (shouldShowRequestPermissionRationale(READ_EXTERNAL_STORAGE)) {
+                                showMessageOKCancel("You need to give permission for the app to work",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                    requestPermissions(new String[]{READ_EXTERNAL_STORAGE,WRITE_EXTERNAL_STORAGE},
+                                                            PERMISSION_REQUEST_CODE);
+                                                }
+                                            }
+                                        });
+                                return;
+                            }
+                        }
+
+                    }
+                }
+
+
+                break;
+        }
+    }
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        PermissionAccessDialog=new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create();
+    }
+
+
+
 }
