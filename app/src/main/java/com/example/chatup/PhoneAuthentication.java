bb08c2da1cda5381a6f6847a15f7b5b199ffdb21
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-07-08 20:03:01 +0530

    java code for phone authentication

---
+++ app/src/main/java/com/example/chatup/PhoneAuthentication.java
@@ -0,0 +1,14 @@
+package com.example.chatup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneAuthentication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_authentication);
+    }
+}
