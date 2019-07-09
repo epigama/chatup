51ab2dac28272c69ff9e9b5d5a7d2911bcaefdc2
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-07-09 19:27:05 +0530

    added timer to the splash screen

--- app/src/main/java/com/example/chatup/IntroPage.java
+++ app/src/main/java/com/example/chatup/IntroPage.java
@@ -4,16 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
 public class IntroPage extends AppCompatActivity {
+    public static final int splash_screen_timer=4000;
     Button intro_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_page);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroPage.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },splash_screen_timer);
         getSupportActionBar().hide();
         intro_button=(Button)findViewById(R.id.IntroButton);
         intro_button.setOnClickListener(new View.OnClickListener() {
