63aa41ce96ac503f9636e6233cb5b9bfe713ba80
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-07-11 19:37:05 +0530

    java code for profile activity

---
+++ app/src/main/java/com/example/chatup/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.chatup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile2);
+        getSupportActionBar().hide();
+    }
+}
