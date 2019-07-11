2b262003805e9a6629838d12b8d0160f9e752445
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-07-11 20:24:31 +0530

    java code to get image from user

--- app/src/main/java/com/example/chatup/Chat.java
+++ app/src/main/java/com/example/chatup/Chat.java
@@ -1,14 +1,74 @@
 package com.example.chatup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Chat extends AppCompatActivity {
+    ImageView user_image_view;
+    static int PReqCode = 1 ;
+    static int REQUESCODE = 1 ;
+    Uri pickedImgUri ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        user_image_view=(ImageView)findViewById(R.id.UserImage);
+        user_image_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= 22) {
+
+                    checkAndRequestForPermission();
+
+
+                }
+                else
+                {
+                    openGallery();
+                }
+            }
+        });
+    }
+    private void checkAndRequestForPermission() {
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                Toast.makeText(this,"Please accept for required permission",Toast.LENGTH_SHORT).show();
+
+            }
+
+            else
+            {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PReqCode);
+            }
+
+        }
+        else
+            openGallery();
+
+    }
+    private void openGallery() {
+        //TODO: open gallery intent and wait for user to pick an image !
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESCODE);
     }
 }
