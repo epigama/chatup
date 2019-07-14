0dad9f22f9ca89058d28883222f658440f3ee0b0
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-07-14 21:11:08 +0530

    code structured

--- app/src/main/java/com/example/chatup/IntroPage.java
+++ app/src/main/java/com/example/chatup/IntroPage.java
@@ -1,7 +1,6 @@
 package com.example.chatup;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,7 +9,6 @@ import android.widget.Button;
 
 public class IntroPage extends AppCompatActivity {
     public static final int splash_screen_timer=2000;
-    Button intro_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +22,11 @@ public class IntroPage extends AppCompatActivity {
             }
         },splash_screen_timer);
         View decorView = getWindow().getDecorView();
-// Hide the status bar.
+        // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
-// Remember that you should never show the action bar if the
-// status bar is hidden, so hide that too if necessary.
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
         androidx.appcompat.app.ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
     }
