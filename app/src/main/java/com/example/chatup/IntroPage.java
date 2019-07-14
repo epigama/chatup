3fbb11d32162c7fa363bed6d463349c4d4186e5a
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-07-14 21:12:00 +0530

    removed button import module

--- app/src/main/java/com/example/chatup/IntroPage.java
+++ app/src/main/java/com/example/chatup/IntroPage.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
+
 
 public class IntroPage extends AppCompatActivity {
     public static final int splash_screen_timer=2000;
