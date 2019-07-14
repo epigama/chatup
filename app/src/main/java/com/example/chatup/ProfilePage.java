e490e0a24ad8b260784e4fd289fec00797db2b18
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-07-14 21:09:21 +0530

    ...

--- app/src/main/java/com/example/chatup/ProfilePage.java
+++ app/src/main/java/com/example/chatup/ProfilePage.java
@@ -5,7 +5,6 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
