ee4ef2e480527a078d9e3d0c0b3f817d0ee41347
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-07-16 19:23:29 +0530

    when clicked setting goes to settings page

--- app/src/main/java/com/example/chatup/Chat.java
+++ app/src/main/java/com/example/chatup/Chat.java
@@ -2,7 +2,13 @@ package com.example.chatup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Chat extends AppCompatActivity {
 
@@ -10,5 +16,22 @@ public class Chat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.Settings) {
+            startActivity(new Intent(this, LoginPage.class));
+            return true;
+        }
+        return true;
     }
 }
