a20c68805a16bfeeea55e0c913f19ab192c516c3
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-07-17 07:36:40 +0530

    moved calls to fragment package

--- app/src/main/java/com/example/chatup/Fragments/Calls.java
+++ app/src/main/java/com/example/chatup/Fragments/Calls.java
@@ -1,4 +1,4 @@
-package com.example.chatup;
+package com.example.chatup.Fragments;
 
 import android.content.Context;
 import android.net.Uri;
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.chatup.R;
 
 
 /**
