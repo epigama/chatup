b53059f15821e949e73a1834a83adf02d606381f
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-08-04 08:08:21 +0530

    modifications

--- app/src/main/java/com/example/chatup/Chats.java
+++ app/src/main/java/com/example/chatup/Chats.java
@@ -112,11 +112,11 @@ public class Chats extends AppCompatActivity {
 
         if(type == 1) {
             lp2.gravity = Gravity.RIGHT;
-            textView.setBackgroundResource(R.drawable.rounded_background_blue);
+            textView.setBackgroundResource(R.drawable.bubble_in);
         }
         else{
             lp2.gravity = Gravity.LEFT;
-            textView.setBackgroundResource(R.drawable.rounded_background_blue);
+            textView.setBackgroundResource(R.drawable.bubble_out);
         }
         textView.setLayoutParams(lp2);
         layout.addView(textView);
