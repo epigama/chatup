e3857ff0a23c08a7a795fbc3ffc3b1e32c0e144a
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-08-07 21:22:52 +0530

    strings

--- app/src/main/java/com/example/chatup/FAQ.java
+++ app/src/main/java/com/example/chatup/FAQ.java
@@ -10,5 +10,6 @@ public class FAQ extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+        getSupportActionBar().setTitle("FAQ");
     }
 }
