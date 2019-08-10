897969a1911c3210cbd7ea2c190cdeae0638d3a0
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-08-10 19:07:22 +0530

    added dialog

--- app/src/main/java/com/example/chatup/FAQ.java
+++ app/src/main/java/com/example/chatup/FAQ.java
@@ -6,16 +6,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.chatup.Dialog.DialogChangeProfile;
+import com.example.chatup.Dialog.DialogSettings;
+import com.example.chatup.Dialog.DialogWriteFeedback;
+
 public class FAQ extends AppCompatActivity {
-    TextView faq;
+    TextView faq_open_settings;
+    TextView faq_change_profile;
+    TextView faq_write_feedback;
+    TextView write_feedback_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
 //        getSupportActionBar().setTitle("FAQ");
-        faq=findViewById(R.id.OpenSettings);
-        faq.setOnClickListener(new View.OnClickListener() {
+        faq_open_settings=findViewById(R.id.OpenSettings);
+        faq_change_profile=findViewById(R.id.ChangeProfile);
+        faq_write_feedback=findViewById(R.id.WriteFeedback);
+        faq_write_feedback=findViewById(R.id.WriteFeedback1);
+
+
+        faq_open_settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 opendialog();
@@ -23,6 +35,30 @@ public class FAQ extends AppCompatActivity {
 
             private void opendialog() {
                 DialogSettings dialog= new DialogSettings();
+                dialog.show(getSupportFragmentManager(),"dialog");
+            }
+        });
+
+        faq_change_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opendialog();
+            }
+
+            private void opendialog() {
+                DialogChangeProfile dialog= new DialogChangeProfile();
+                dialog.show(getSupportFragmentManager(),"dialog");
+            }
+        });
+
+        faq_write_feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opendialog();
+            }
+
+            private void opendialog() {
+                DialogWriteFeedback dialog= new DialogWriteFeedback();
                 dialog.show(getSupportFragmentManager(),"dialog");
             }
         });
