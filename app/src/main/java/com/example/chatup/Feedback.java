434ad191fab97f703629c4469ae472d1a9480689
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-08-31 17:20:38 +0530

    added email for feedback

--- app/src/main/java/com/example/chatup/Feedback.java
+++ app/src/main/java/com/example/chatup/Feedback.java
@@ -2,13 +2,29 @@ package com.example.chatup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Feedback extends AppCompatActivity {
+    Button feedback_mail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        feedback_mail=findViewById(R.id.FeedbackMail);
+        feedback_mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto","bhavnaharitsa@gmail.com", null));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+                intent.putExtra(Intent.EXTRA_TEXT, "My feedback is   ");
+                startActivity(Intent.createChooser(intent, "Choose an Email client :"));
+            }
+        });
     }
 }
