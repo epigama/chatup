e550b5dd5a14e728802771ecd6ffda910ba7142a
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-09-20 15:46:49 +0530

    added new activity

---
+++ app/src/main/java/com/example/chatup/Contact_detail.java
@@ -0,0 +1,14 @@
+package com.example.chatup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Contact_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_detail);
+    }
+}
