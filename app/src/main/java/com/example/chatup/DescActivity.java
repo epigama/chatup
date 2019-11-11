94c0ef06553a667cb9c0aee5ef67056bcd194e84
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-11-11 11:33:54 +0530

    new activity for intro slides

--- app/src/main/java/com/example/chatup/DescActivity.java
+++ app/src/main/java/com/example/chatup/DescActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DescActivity extends AppCompatActivity {
+import com.github.paolorotolo.appintro.AppIntro;
+
+public class DescActivity extends AppIntro {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_desc);
+
     }
 }
