895d61850a834ede35a70c8012393c4329fd29fe
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-11-30 05:48:57 +0530

    implemented search feature

--- app/src/main/java/com/example/chatup/Fragments/Users.java
+++ app/src/main/java/com/example/chatup/Fragments/Users.java
@@ -4,23 +4,30 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -29,123 +36,169 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.chatup.Activities.Chats;
+import com.example.chatup.Adapters.UserAdapter;
 import com.example.chatup.Models.User;
 import com.example.chatup.R;
 import com.example.chatup.Models.UserDetails;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+
+import butterknife.OnItemClick;
 
 public class Users extends Fragment {
-    int currentdaynight;
-    ListView usersList;
-    TextView noUsersText;
-    TextView noUsersText2;
-    ArrayList<String> al = new ArrayList<>();
-    ArrayList<String> usersAl = new ArrayList<>();
-    int totalUsers = 0;
-    ProgressDialog pd;
+    private RecyclerView recyclerView;
 
-    @Nullable
+    Typeface MR, MRR;
+    FrameLayout frameLayout;
+    TextView es_descp, es_title;
+
+    private UserAdapter userAdapter;
+    private List<User> mUsers;
+    static OnItemClick onItemClick;
+
+    EditText search_users;
+
+    public static Users newInstance(OnItemClick click) {
+        onItemClick = click;
+        Bundle args = new Bundle();
+
+        Users fragment = new Users();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.activity_users, container, false);
-        usersList = (ListView) view.findViewById(R.id.usersList);
-        noUsersText = (TextView) view.findViewById(R.id.noUsersText);
-        noUsersText2 = view.findViewById(R.id.invite_folks);
-        pd = new ProgressDialog(getContext());
-        pd.setMessage("Loading...");
-        pd.show();
-        currentdaynight = AppCompatDelegate.getDefaultNightMode();
-        String url = "https://chaton-343f1.firebaseio.com/users.json";
-        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
+
+//        MRR = Typeface.createFromAsset(getContext().getAssets(), "fonts/myriadregular.ttf");
+//        MR = Typeface.createFromAsset(getContext().getAssets(), "fonts/myriad.ttf");
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        frameLayout = view.findViewById(R.id.es_layout);
+        es_descp = view.findViewById(R.id.es_descp);
+        es_title = view.findViewById(R.id.es_title);
+
+        es_descp.setTypeface(MR);
+        es_title.setTypeface(MRR);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        mUsers = new ArrayList<>();
+
+        readUsers();
+
+        search_users = view.findViewById(R.id.search_users);
+        search_users.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onResponse(String s) {
-                doOnSuccess(s);
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
             }
-        }, new Response.ErrorListener() {
+
             @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                System.out.println("" + volleyError);
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                searchUsers(charSequence.toString().toLowerCase());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
 
-        RequestQueue rQueue = Volley.newRequestQueue(getContext());
-        rQueue.add(request);
-
-        usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                UserDetails.setChatWith(al.get(position));
-                UserDetails.setChatWithNumber(al.get(position));
-                startActivity(new Intent(getContext(), Chats.class));
-            }
-        });
         return view;
     }
 
+    private void searchUsers(String s) {
 
-    public void doOnSuccess(String s) {
-        try {
-            JSONObject obj = new JSONObject(s);
+        final FirebaseUser fuser = FirebaseAuth.getInstance().getCurrentUser();
+        Query query = FirebaseDatabase.getInstance().getReference("Users").orderByChild("search")
+                .startAt(s)
+                .endAt(s+"\uf8ff");
 
-            Iterator i = obj.keys();
-            String key = "";
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mUsers.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
 
-            while (i.hasNext()) {
-                key = i.next().toString();
-                if (!key.equals(UserDetails.phoneNum)) {
-                    al.add(key);
-                    String contactName = getContactName(getContext(), key);
-                    if(contactName != ""){
-                        Log.d("", "onItemClick: " + "Contact exists");
-                        usersAl.add(contactName);
+                    assert user != null;
+                    assert fuser != null;
+                    if (!user.getId().equals(fuser.getUid())){
+                        mUsers.add(user);
                     }
                 }
-                totalUsers++;
+
+                userAdapter = new UserAdapter(getContext(),onItemClick, mUsers, false);
+                recyclerView.setAdapter(userAdapter);
             }
 
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        try {
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-            if (totalUsers <= 1) {
-                noUsersText.setVisibility(View.VISIBLE);
-                usersList.setVisibility(View.GONE);
-            } else {
-                noUsersText.setVisibility(View.GONE);
-                noUsersText2.setVisibility(View.GONE);
-                usersList.setVisibility(View.VISIBLE);
-                usersList.setAdapter(new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, usersAl));
             }
+        });
 
-            pd.dismiss();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
-    public String getContactName(Context context, String number) {
-        Uri lookupUri = Uri.withAppendedPath(
-                ContactsContract.PhoneLookup.CONTENT_FILTER_URI,
-                Uri.encode(number));
-        String[] mPhoneNumberProjection = { ContactsContract.PhoneLookup._ID, ContactsContract.PhoneLookup.NUMBER, ContactsContract.PhoneLookup.DISPLAY_NAME };
-        Cursor cur = context.getContentResolver().query(lookupUri,mPhoneNumberProjection, null, null, null);
-        try {
-            if (cur.moveToFirst()) {
-                String name = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                return name;
+    private void readUsers() {
+
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (search_users.getText().toString().equals("")) {
+                    mUsers.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        User user = snapshot.getValue(User.class);
+
+                        if (!user.getId().equals(firebaseUser.getUid())) {
+                            mUsers.add(user);
+                        }
+                    }
+
+                    if(mUsers.size()==0){
+                        frameLayout.setVisibility(View.VISIBLE);
+                    }
+                    else{
+                        frameLayout.setVisibility(View.GONE);
+                    }
+
+                    userAdapter = new UserAdapter(getContext(), onItemClick,mUsers, false);
+                    recyclerView.setAdapter(userAdapter);
+                }
             }
-        } finally {
-            if (cur != null)
-                cur.close();
-        }
-        return "";
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
-}
+    }
+
+
 
 
 
